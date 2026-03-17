4c381aad3e4dab004b6aaece51ce909c55ed1907
Sadam Ahmad <sadamahmadn11@gmail.com>
2026-03-17 13:47:01 +0700

    Pengerjaan Live Session 5

--- app/(dashboard)/components/layouts/sidebar.tsx
+++ app/(dashboard)/components/layouts/sidebar.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { logout } from "@/app/services/auth.service";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 import {
   FiBox,
   FiCreditCard,
@@ -13,6 +15,8 @@ import {
 
 const Sidebar = () => {
   const pathname = usePathname();
+  const { push } = useRouter();
+
   const menuItems = [
     {
       name: "Products",
@@ -35,6 +39,11 @@ const Sidebar = () => {
       link: "/admin/bank-info",
     },
   ];
+
+  const handleLogout = () => {
+    logout();
+    push("/admin/login");
+  };
 
   return (
     <aside className="w-80 min-h-screen bg-white border-r border-gray-100 flex flex-col fixed left-0 top-0">
@@ -62,12 +71,12 @@ const Sidebar = () => {
           );
         })}
       </div>
-      <Link
-        href="#"
-        className="flex gap-3 font-medium py-3 px-4.5 mx-5 hover:bg-gray-100 duration-300 rounded-lg mt-auto mb-10">
+      <button
+        className="flex cursor-pointer gap-3 font-medium py-3 px-4.5 mx-5 hover:bg-gray-100 duration-300 rounded-lg mt-auto mb-10"
+        onClick={handleLogout}>
         <FiLogOut size={24} />
         Log Out
-      </Link>
+      </button>
     </aside>
   );
 };
